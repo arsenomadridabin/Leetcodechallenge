706b61f12c2e4b7aabf1ef2161304cbe75ef98be
Abin Shakya <arsenomadridabin@hotmail.com>
2021-09-08 15:47:55 -0500

    Optimum uploaded

--- problem1.py
+++ problem1.py
@@ -1,6 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        new_nums = nums.copy()
+        new_nums.sort()
+        largest = new_nums[len(new_nums)-1]
+        smallest = new_nums[0]
         for i,value in enumerate(nums):
+            if value + largest < target or value + smallest > target:
+                continue
+            
             next_num = target - value
             nums_copy = nums.copy()
             data = value in nums_copy
@@ -10,3 +17,4 @@ class Solution:
                     return [i,nums_copy.index(next_num)+1]
                 else:
                     return [i,nums_copy.index(next_num)]
+
